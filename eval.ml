a8b31b384297384ef82c96b48e6148b17c1260ee
YugyeomKim <hoodie0512@gmail.com>
2024-03-29 02:21:47 +0900

    Fix:  Ultimate answer
    1. Swap properly for lamda abs
    2. Fix substitution order
    3. Reduce Lam more
    4. Reduce Var Exp more
    5. Merge all App cases

--- eval.ml
+++ eval.ml
@@ -28,10 +28,11 @@ let rec fV (e : Uml.exp) =
  *)
 let rec variableSwap (x : Uml.var) (y : Uml.var) (e : Uml.exp) =
   match e with
-  | Uml.Var z -> if z = x then Uml.Var y else Uml.Var z
+  | Uml.Var z ->
+      if z = x then Uml.Var y else if z = y then Uml.Var x else Uml.Var z
   | Uml.Lam (z, e) ->
-      if z = x then Uml.Lam (z, e)
-      else if z = y then Uml.Lam (z, e)
+      if z = x then Uml.Lam (y, variableSwap x y e)
+      else if z = y then Uml.Lam (x, variableSwap x y e)
       else Uml.Lam (z, variableSwap x y e)
   | Uml.App (e1, e2) -> Uml.App (variableSwap x y e1, variableSwap x y e2)
 
@@ -49,8 +50,6 @@ let getNewVariableNotInList lst =
  * implement the substitution function [e'/x]e.
  *)
 let rec substitute (e' : Uml.exp) (x : string) (e : Uml.exp) =
-  (* Printf.printf "===sub: %s | %s | %s\n" (Inout.exp2string e') x
-     (Inout.exp2string e); *)
   match (e', x, e) with
   | e', x, Uml.Var y -> if x = y then e' else Uml.Var y
   | e', x, Uml.App (e1, e2) -> Uml.App (substitute e' x e1, substitute e' x e2)
@@ -58,22 +57,18 @@ let rec substitute (e' : Uml.exp) (x : string) (e : Uml.exp) =
       if x = y then Uml.Lam (y, e)
       else if List.mem y (fV e') then
         let z = getNewVariableNotInList ([ x ] @ [ y ] @ fV e' @ fV e) in
-        Uml.Lam (z, substitute (variableSwap z y e') x e)
+        Uml.Lam (z, substitute e' x (variableSwap y z e))
       else Uml.Lam (y, substitute e' x e)
 
 (*
  * implement a single step with reduction using the call-by-value strategy.
  *)
 let rec stepv e : Uml.exp =
-  (* Printf.printf "===Value of e: %s\n" (Inout.exp2string e); *)
   match e with
   | Uml.Var x -> raise Stuck
-  | Uml.Lam (x, e) -> raise Stuck
-  | Uml.App (Uml.Var x, e2) -> raise Stuck
-  | Uml.App (Uml.Lam (x, e1), Uml.Var y) -> substitute (Uml.Var y) x e1
-  | Uml.App (Uml.Lam (x, e1), Uml.Lam (y, e2)) ->
-      substitute (Uml.Lam (y, e2)) x e1
-  | Uml.App (Uml.Lam (x, e1), e2) -> Uml.App (Uml.Lam (x, e1), stepv e2)
+  | Uml.Lam (x, e) -> Uml.Lam (x, stepv e)
+  | Uml.App (Uml.Var x, e2) -> Uml.App (Uml.Var x, stepv e2)
+  | Uml.App (Uml.Lam (x, e1), e2) -> substitute e2 x e1
   | Uml.App (e1, e2) -> Uml.App (stepv e1, e2)
 
 let stepOpt stepf e = try Some (stepf e) with Stuck -> None
